12d798ce5d3830a7e19505b3324baa0da087fedd
agorbacheva-y <agorbacheva.y@gmail.com>
2023-09-27 22:41:29 +0200

    Comment.js basic structure

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -3,7 +3,26 @@ import React from 'react'
 function Comment() {
   return (
     <div className='comment'>
-      <p>username</p>
+      <div className='comment__header'>
+        <p>avatar</p>
+        <p>username</p>
+        <p>badge if you == true</p>
+        <p>date posted</p>
+      </div>
+
+      <div className='comment__body'>
+        <p>comment</p>
+      </div>
+
+      <div className='comment__footer'>
+        <div className='comment__footer--likes'>
+          <p>likes</p>
+        </div>
+        <div className='comment__footer--icons'>
+          <p>delete icon</p>
+          <p>edit icon</p>
+        </div>
+      </div>
     </div>
   );
 };
